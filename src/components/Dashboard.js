c6db8109cb5f067301355edcc71ff4465d3b49f2
RicardoG2016 <gomez.r@live.com>
2017-04-11 17:36:15 -0500

    setup bootstrap and navbar

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="dash">
+        <button></button>
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
