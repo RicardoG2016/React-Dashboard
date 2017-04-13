fcd0d83e6a6477ae9d5abac21071b25896a529c5
RicardoG2016 <gomez.r@live.com>
2017-04-13 17:20:53 -0500

    navbar-add additional icons

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -24,29 +24,39 @@ export default class NavBar extends React.Component {
 
                         <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                           <ul className="nav navbar-nav">
-                            <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
-                            <li><a href="#">Link</a></li>
+                            <li className="active"><a href="#"><img className="img" src="../images/alarm.png" alt="Generic placeholder image" width="20" height="20" /> <span className="sr-only">(current)</span></a></li>
+                            <li><a href="#"><img className="img" src="../images/gift.png" alt="Generic placeholder image" width="20" height="20" /></a></li>
                             <li className="dropdown">
-                              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                              <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><img className="img" src="../images/m.png" alt="Generic placeholder image" width="20" height="20" /> </a>
                               <ul className="dropdown-menu" role="menu">
-                                <li><a href="#">Action</a></li>
-                                <li><a href="#">Another action</a></li>
-                                <li><a href="#">Something else here</a></li>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Account</a></li>
                                 <li className="divider"></li>
-                                <li><a href="#">Separated link</a></li>
+                                <li><a href="#">Review</a></li>
+                                <li><a href="#">Social</a></li>
+                                <li><a href="#">Jobs</a></li>
+                                <li><a href="#">Projects</a></li>
+                                <li><a href="#">Docs</a></li>
+                                <li><a href="#">References</a></li>
                                 <li className="divider"></li>
-                                <li><a href="#">One more separated link</a></li>
+                                <li><a href="#">Gifts</a></li>
+                                <li><a href="#">Premium</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">Settings</a></li>
                               </ul>
                             </li>
+                            <form className="navbar-form navbar-left" role="search">
+                              <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Search" />
+                              </div>
+                              <button type="submit" className="btn btn-default">Submit</button>
+                            </form>
                           </ul>
-                          <form className="navbar-form navbar-left" role="search">
-                            <div className="form-group">
-                              <input type="text" className="form-control" placeholder="Search" />
-                            </div>
-                            <button type="submit" className="btn btn-default">Submit</button>
-                          </form>
+
                           <ul className="nav navbar-nav navbar-right">
-                            <li><a href="#">Signin</a></li>
+                            <li><a href="#">Sign In</a></li>
+                            <li><a href="#">|</a></li>
+                            <li><a href="#">Sign Up</a></li>
                           </ul>
                         </div>
                       </div>
